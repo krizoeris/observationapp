976e832c33b83cba948ade7840456798a3fb859e
krizoeris <krizoeris@yahoo.com.ph>
2020-05-07 10:55:00 +0400

    Grades delete option added

--- src/components/admin/grades/GradeDelete.js
+++ src/components/admin/grades/GradeDelete.js
@@ -1,9 +1,37 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faExclamationTriangle, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { NotificationManager } from 'react-notifications';
+import { Button } from 'reactstrap';
 
-const GradeDelete = () => {
+const GradeDelete = ({id, toggle, name, loadGrades}) => {
+    const deleteGradeData = async () => {
+        const deleteGrade = await fetch(`${process.env.REACT_APP_BACKEND_URL}/grades`, {
+            method: 'DELETE',
+            headers: {"Content-type": "application/json"},
+            body: JSON.stringify({
+                id: id
+            })
+        })
+        const response = await deleteGrade.json()
+        
+        if(response.success) {
+            NotificationManager.warning(`${name}`, 'Successfully Deleted!');
+            loadGrades()
+            toggle()
+        } else {
+            NotificationManager.error('There has been a problem deleting', 'Error');
+            toggle()
+        }
+    }
+    
     return (
         <div>
-            grade delete
+            <p className="text-center"><FontAwesomeIcon icon={faExclamationTriangle} /> Are you sure you want to delete <strong>{name}</strong>?</p>
+            <div className="d-flex justify-content-center mt-2">
+                <Button className="mr-1 ml-1" onClick={deleteGradeData} color="danger"><FontAwesomeIcon icon={faTrash} /> Delete</Button>
+                <Button className="mr-1 ml-1" onClick={() => toggle()} color="secondary">Cancel</Button>
+            </div>
         </div>
     )
 }
--- src/components/admin/subjects/SubjectDelete.js
+++ src/components/admin/subjects/SubjectDelete.js
@@ -5,15 +5,15 @@ import { NotificationManager } from 'react-notifications';
 import { Button } from 'reactstrap';
 
 const SubjectDelete = ({id, toggle, name, loadSubjects}) => {
-    const deleteUserData = async () => {
-        const deleteUser = await fetch(`${process.env.REACT_APP_BACKEND_URL}/subjects`, {
+    const deleteSubjectData = async () => {
+        const deleteSubject = await fetch(`${process.env.REACT_APP_BACKEND_URL}/subjects`, {
             method: 'DELETE',
             headers: {"Content-type": "application/json"},
             body: JSON.stringify({
                 id: id
             })
         })
-        const response = await deleteUser.json()
+        const response = await deleteSubject.json()
         
         if(response.success) {
             NotificationManager.warning(`${name}`, 'Successfully Deleted!');
@@ -29,7 +29,7 @@ const SubjectDelete = ({id, toggle, name, loadSubjects}) => {
         <div>
             <p className="text-center"><FontAwesomeIcon icon={faExclamationTriangle} /> Are you sure you want to delete <strong>{name}</strong>?</p>
             <div className="d-flex justify-content-center mt-2">
-                <Button className="mr-1 ml-1" onClick={deleteUserData} color="danger"><FontAwesomeIcon icon={faTrash} /> Delete</Button>
+                <Button className="mr-1 ml-1" onClick={deleteSubjectData} color="danger"><FontAwesomeIcon icon={faTrash} /> Delete</Button>
                 <Button className="mr-1 ml-1" onClick={() => toggle()} color="secondary">Cancel</Button>
             </div>
         </div>
